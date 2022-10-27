5202d7522e71d70c5b49c17c96d398d0b026cd35
theimpostor <theimpostor@users.noreply.github.com>
2022-10-27 08:07:37 -0500

    some basic utilities

--- go/main.go
+++ go/main.go
@@ -1,6 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func abs(a int) int {
+	if a < 0 {
+		return a * -1
+	}
+	return a
+}
+
+func sum(a ...int) (r int) {
+	for _, n := range a {
+		r += n
+	}
+	return
+}
+
+func max(a ...int) (r int) {
+	r = math.MinInt
+	for i := range a {
+		if a[i] > r {
+			r = a[i]
+		}
+	}
+	return
+}
+
+func min(a ...int) (r int) {
+	r = math.MaxInt
+	for i := range a {
+		if a[i] < r {
+			r = a[i]
+		}
+	}
+	return
+}
 
 func main() {
 	fmt.Println("hello world")
