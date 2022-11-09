a32ecf6c2ecdd42f78634e8eff722d4320f7d957
theimpostor <theimpostor@users.noreply.github.com>
2022-11-08 19:11:12 -0600

    Add stack + dupSlice

--- go/main.go
+++ go/main.go
@@ -5,6 +5,36 @@ import (
 	"math"
 )
 
+type stack struct {
+    s []int
+}
+
+func (s *stack) push(v int) {
+    s.s = append(s.s, v)
+}
+
+func (s *stack) pop() int {
+    v := s.peek()
+    s.s = s.s[:len(s.s)-1]
+    return v
+}
+
+func (s *stack) peek() int {
+    return s.s[len(s.s)-1]
+}
+
+func (s *stack) empty() bool {
+    return len(s.s) == 0
+}
+
+func (s *stack) size() int {
+    return len(s.s)
+}
+
+func dupSlice(s []int) []int {
+	return append(make([]int, 0, len(s)), s...)
+}
+
 func abs(a int) int {
 	if a < 0 {
 		return a * -1
