f245bdefbf18c0712b69e0e28ada1e3cf48e7173
theimpostor <sahirhoda@gmail.com>
2025-07-17 05:34:31 -0500

    python template

---
+++ python/template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "requests",
+# ]
+# ///
+
+
+def main() -> None:
+    print("Hello from template.py!")
+
+
+if __name__ == "__main__":
+    main()
